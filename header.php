7d95ac940e3ca8c6609ed36ab57467945dc41f11
DESKTOP-V60C6C8\Carlo <carl.fontanos@gmail.com>
2017-01-04 23:22:58 +0800

    Finished header top 2, nav bar and the banner is partially done.

--- header.php
+++ header.php
@@ -28,5 +28,52 @@
 					<input type="submit" value="Login" class="a-s" />
 				</div>
 			</div>
+			
+			<div class="top-2 clearfix">
+				<a href="_#" class="fl-l"><img src="img/logo.png" class="logo img-responsive" /></a>
+				<a href="_#" class="fl-r"><img src="img/promo-01.jpg" class="img-responsive a-o" /></a>
+			</div>
+			
+			<nav class="a-s">
+				<ul>
+					<li><a href="_#">Home</a></li
+					><li><a href="_#">About</a></li
+					><li><a href="_#">Affiliates</a></li
+					><li><a href="_#">Games</a></li
+					><li><a href="_#">News</a></li
+					><li><a href="_#">Forums</a></li
+					><li><a href="_#">Feeds</a></li
+					><li><a href="_#">FAQ</a></li
+					><li><a href="_#">Contact</a></li>
+				</ul>
+			</nav>
+			
+			<div class="banner a-s">
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="img/slider-logo.png" class="dollar" />
+							<img src="img/slider-1.jpg" class="background" />
+							<div class="message">
+								<h3>Welcome to our</h3>
+								<h1>Soft Launch</h1>
+								<h4>Get ahead of everyone else & earn cash by signing <br />up as a founder of MMOCashout today!</h4>
+							</div>
+						</div>
+						<div class="item">
+							<img src="img/slider-1.jpg" />
+						</div>
+					</div>
+					
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
 		</div>
 	</header>
