28dac14f5f48b956ccf310bc4d0afc4e462d5fd9
Carlo Fontanos <carl.fontanos@gmail.com>
2017-01-04 18:39:12 +0800

    Finished working on the header top part where the select language and login/register form is located.

--- header.php
+++ header.php
@@ -6,9 +6,27 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
+	<link rel="stylesheet" href="lib/bootstrap/bootstrap.min.css">	
 	<link rel="stylesheet" href="css/helpers.css">
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body class="override">
+	<header class="container">
+		<div class="row">
+			<div class="top clearfix">
+				<select class="a-s">
+					<option>Choose Your Language</option>
+					<option>English</option>
+					<option>Japanese</option>
+					<option>Chinese</option>
+				</select>
+				<div class="login-form">
+					<input type="text" placeholder="Enter Your Name" class="a-s" />
+					<input type="text" placeholder="Enter Email Address" class="a-s" />
+					<input type="submit" value="Register" class="a-s" />
+					<input type="submit" value="Login" class="a-s" />
+				</div>
+			</div>
+		</div>
+	</header>
