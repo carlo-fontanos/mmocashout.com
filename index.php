ce22af4443191af9af09c0e543a4f5e0cbf2eeca
Carlo Fontanos <carl.fontanos@gmail.com>
2017-01-05 18:01:51 +0800

    Added "Featured Games" title bar.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php require_once('header.php'); ?>
 
-<div class="container">
-	
+<div class="container content">
+	<div class="featured-games">
+		<h2>Featured Games</h2>
+	</div>
 </div>
 	
 <?php require_once('footer.php'); ?>
