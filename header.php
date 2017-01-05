8ca69621d3bc662aed6761fc5b487015c6447af5
Carlo Fontanos <carl.fontanos@gmail.com>
2017-01-05 18:23:08 +0800

    Changed ID name.

--- header.php
+++ header.php
@@ -53,7 +53,7 @@
 			</nav>
 			
 			<div class="banner a-s">
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<div id="banner-slider" class="carousel slide" data-ride="carousel">
 					<div class="carousel-inner" role="listbox">
 						<div class="item active">
 							<img src="img/slider-logo.png" class="dollar" />
@@ -78,11 +78,11 @@
 						-->
 					</div>
 					
-					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+					<a class="left carousel-control" href="#banner-slider" role="button" data-slide="prev">
 						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 						<span class="sr-only">Previous</span>
 					</a>
-					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+					<a class="right carousel-control" href="#banner-slider" role="button" data-slide="next">
 						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 						<span class="sr-only">Next</span>
 					</a>
