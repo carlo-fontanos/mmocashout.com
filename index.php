df0feda17ea83d494c45cf07639039acded1e2da
Carlo Fontanos <carl.fontanos@gmail.com>
2017-01-06 17:06:54 +0800

    Finished building out section 1 and section 2.

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php require_once('header.php'); ?>
 
 <div class="container content">
-	<div class="featured-games">
-		<h2>Featured Games</h2>
+	<div class="featured-games section">
+		<h2 class="title">Featured Games</h2>
 		
 		<div id="featured-games-slider" class="carousel slide" data-ride="carousel">
 			<div class="carousel-inner" role="listbox">
 				<div class="item active text-center">
-					<div class="col-sm-3">
-						<img src="img/carousel-1.jpg">
-						<a href="#" class="link">Visit Site</a>
+					<div class="col-md-3 col-sm-6">
+						<img src="img/carousel-1.jpg" class="a-s a-s-h" />
+						<a href="#" class="link-btn">Visit Site</a>
 					</div>
-					<div class="col-sm-3">
-						<img src="img/carousel-2.jpg">
-						<a href="#" class="link">Visit Site</a>
+					<div class="col-md-3 col-sm-6">
+						<img src="img/carousel-2.jpg" class="a-s a-s-h" />
+						<a href="#" class="link-btn">Visit Site</a>
 					</div>
-					<div class="col-sm-3">
-						<img src="img/carousel-3.jpg">
-						<a href="#" class="link">Visit Site</a>
+					<div class="col-md-3 col-sm-6">
+						<img src="img/carousel-3.jpg" class="a-s a-s-h" />
+						<a href="#" class="link-btn">Visit Site</a>
 					</div>
-					<div class="col-sm-3">
-						<img src="img/carousel-4.jpg">
-						<a href="#" class="link">Visit Site</a>
+					<div class="col-md-3 col-sm-6">
+						<img src="img/carousel-4.jpg" class="a-s a-s-h" />
+						<a href="#" class="link-btn">Visit Site</a>
 					</div>
 				</div>
 				<!--
@@ -50,6 +50,118 @@
 				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
+		</div>
+	</div>
+	
+	<div class="clearfix section-1 ml-t">
+		<div class="col-sm-6 p-r p-r-rm">
+			<h2 class="title m-0">MMOCashout Opportunities</h2>
+			<div class="section pl-d">
+				<img src="img/matrix.png" class="fl-l ml-r" />
+				<p>We have this golden opportunity for you to earn money with your favorite MMO (Massively Multiplayer Online) game through our unique business plan!</p>
+				<p>Get to buy and sell items of your liking in our online store - powerful swords, valuable items, rare game merchandise, gold… you name it. You can bid farewell to the long search for the perfect online store, because we have it right here for you. With MMOBuyIt.com, you will have your very own online store for all your gaming (and monetary) needs!</p> 
+				<a href="#" class="fc-w d-ib m-t">LEARN MORE &nbsp; <img src="img/arrow.png" /></a>
+			</div>
+		</div>
+		
+		<div class="col-sm-6 p-l p-l-rm">
+			<h2 class="title m-0">MMOCashout Matrix</h2>
+			<div class="section pl-d">
+				<img src="img/opportunities.png" class="fl-l ml-r" />
+				<p>There is one thing a gamer like you is known to be good at, regardless of how high the level of your characters are. It’s inviting other gamers to join in the fun! </p>
+				<p>MMO Cashout offers a unique compensation plan that allows you to earn by doing just that! As your network grows, you will be paid for every time you would personally sponsor an affiliate to your matrix. Every time one of your affiliates would bring in their own personally sponsored affiliates – depending on the package both of you earn as well.</p>
+				<a href="#" class="fc-w d-ib m-t">LEARN MORE &nbsp; <img src="img/arrow.png" /></a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="clearfix section-2 ml-t">
+		<div class="col-sm-3">
+			<h2 class="title t-sm m-0 ">Affiliate Packages</h2>
+			<div class="section packages p-d">
+				<p>Choose among our affiliate programs we have to offer – be a <span class="fc-br">Squatter</span>, a <span class="fc-br">Peasant</span>, a <span class="fc-br">Squire</span> or a <span class="fc-br">Royal</span> (in classic video gaming terminology) in the MMOCashout Kingdom and Reap the benefits we have in store for you!</p>
+				
+				<a href="#"><img src="img/package-1.jpg" class="m-b a-s a-o" width="100%" /></a>
+				<a href="#"><img src="img/package-2.jpg" class="m-b a-s a-o" width="100%" /></a>
+				<a href="#"><img src="img/package-3.jpg" class="m-b a-s a-o" width="100%" /></a>
+				<a href="#"><img src="img/package-4.jpg" class="m-b a-s a-o" width="100%" /></a>
+				
+				<center>
+					<a href="#" class="link-btn">JOIN NOW!</a>
+				</center>
+			</div>
+			<a href="#"><img src="img/promo-03.jpg" class="ml-t" width="100%" /></a>
+		</div>
+		<div class="col-sm-6 pl-lr pl-lr-rm">
+			<h2 class="title m-0">MMOCashout Forums</h2>
+			<div class="section p-d">
+				<p>Hands on: Extraction – PAX Prime 2013</p>
+				<p>Going Deep: Our First Look At LOTRO’s Upcoming Expansion</p>
+				<p>Hardware: Shipbreakers Scores Homeworld IP, Dev Help From Gearbox</p>
+				<a href="#" class="fc-w d-ib m-t">SEE MORE FEEDS &nbsp; <img src="img/arrow.png" /></a>
+			</div>
+			
+			<a href="#"><img src="img/promo-02.jpg" width="100%" class="ml-t" /></a>
+			
+			<h2 class="title m-0 ml-t">In the News</h2>
+			<div class="section news p-d">
+				<div class="">
+					<img src="img/new-01.jpg" class="fl-l ml-r ml-r-rm" />
+					<h4 class="fc-br">MMO War Thunder coming to PS4</h4>
+					<p>Gaijin Entertainment is bringing its military-themed massively multiplayer online game War Thunder to the PlayStation 4 this holiday, the publisher announced today.</p>
+					<a href="#" class="fc-g">READ ON</a>
+				</div>
+				<hr />
+				<div class="">
+					<img src="img/new-02.jpg" class="fl-l ml-r ml-r-rm" />
+					<h4 class="fc-br">MMO War Thunder coming to PS4</h4>
+					<p>Gaijin Entertainment is bringing its military-themed massively multiplayer online game War Thunder to the PlayStation 4 this holiday, the publisher announced today.</p>
+					<a href="#" class="fc-g">READ ON</a>
+				</div>
+				<hr />
+				<div class="">
+					<img src="img/new-03.jpg" class="fl-l ml-r ml-r-rm" />
+					<h4 class="fc-br">MMO War Thunder coming to PS4</h4>
+					<p>Gaijin Entertainment is bringing its military-themed massively multiplayer online game War Thunder to the PlayStation 4 this holiday, the publisher announced today.</p>
+					<a href="#" class="fc-g">READ ON</a>
+				</div>
+				<hr />
+				<a href="#" class="fc-w d-ib m-t">READ MORE NEWS &nbsp; <img src="img/arrow.png" /></a>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<h2 class="title t-sm m-0">Blog Categories</h2>
+			<div class="section blog p-d">
+				<h5><a href="#" class="fc-w">General: 38 Studios’ IP Auction Coming in November</a></h5>
+				<p class="fc-bl">Posted by chaintm in General Discussion » News & Features Discussion</p>
+				<strong class="text-right fc-br">4m ago</strong>
+				<hr />
+				
+				<h5><a href="#" class="fc-w">Star Trek online</a></h5>
+				<p class="fc-bl">Posted by DamonVile in Star Trek Online » General Discussion</p>
+				<strong class="text-right fc-br">10m ago</strong>
+				<hr />
+				
+				<h5><a href="#" class="fc-w">Is the Beta real?</a></h5>
+				<p class="fc-bl">Posted by Dakeru in WildStar » General Discussion</p>
+				<strong class="text-right fc-br">15m ago</strong>
+				<hr />
+				
+				<h5><a href="#" class="fc-w">What is your GREATEST ACCOMPLISHMENT in your own MMORPG history?</a></h5>
+				<p class="fc-bl">Posted by tommygunzII in General Discussion » The Pub at MMORPG.COM</p>
+				<strong class="text-right fc-br">15m ago</strong>
+				<hr />
+				
+				<h5><a href="#" class="fc-w">Next Expansion to Make or Break WoW</a></h5>
+				<p class="fc-bl">Posted by Hulluck in World of Warcraft » General Discussion</p>
+				<strong class="text-right fc-br">16m ago</strong>
+			</div>
+			
+			<h2 class="title t-sm m-0 ml-t">Join Now!</h2>
+			<div class="section join">
+				<img src="img/dragon.gif" width="100%" />
+			</div>
 		</div>
 	</div>
 </div>
