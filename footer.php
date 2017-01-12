9bb6e0441179080486836afc133e75fd1724b130
Carlo Fontanos <carl.fontanos@gmail.com>
2017-01-12 15:47:03 +0800

    Removed https:

--- footer.php
+++ footer.php
@@ -66,7 +66,7 @@
 		</footer>
 	</div>
 	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="lib/bootstrap/bootstrap.min.js"></script>
 	<script src="js/global.js"></script>
 	<script src="js/app.js"></script>
